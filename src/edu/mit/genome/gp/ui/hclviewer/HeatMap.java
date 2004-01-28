bf502de2f79edad18db0f79d49587190c7dafa58
jgould <jgould@47fccdf2-e235-0410-87e5-63258e95d6e5>
2004-01-28 21:03:31 +0000

    fixed command line processing
    git-svn-id: https://svn.broadinstitute.org/gp2/trunk/modules/HierarchicalClusteringViewer@1269 47fccdf2-e235-0410-87e5-63258e95d6e5

--- src/edu/mit/genome/gp/ui/hclviewer/HeatMap.java
+++ src/edu/mit/genome/gp/ui/hclviewer/HeatMap.java
@@ -13,7 +13,7 @@ public class HeatMap {
 		//	DatasetReader reader = DatasetIO.getReaderByFileName(args[0]);
 		//Dataset d = reader.read(args[0]);
 		Dataset d = Decoder.decodeDataset(args[0]);
-		
+
 		double min = Double.MAX_VALUE;
 		double max = Double.MIN_VALUE;
 		for(int i = 0, rows = d.getRowDimension(); i < rows; i++) {
@@ -24,74 +24,71 @@ public class HeatMap {
 			}
 		}
 		final HCL hcl = new HCL(d, min, max, null, null);
-		org.apache.batik.transcoder.image.ImageTranscoder transcoder = null; 
-        // createImage();
-        String outputFileName = args[1];
-        String outputFormat = args[2];
-        if(outputFormat.equals("png")) {
-        	transcoder = new org.apache.batik.transcoder.image.PNGTranscoder();
+		org.apache.batik.transcoder.image.ImageTranscoder transcoder = null;
+		// createImage();
+		String outputFileName = args[1];
+		String outputFormat = args[2];
+		if(outputFormat.equals("png")) {
+			transcoder = new org.apache.batik.transcoder.image.PNGTranscoder();
 			if(!outputFileName.toLowerCase().endsWith(".png")) {
 				outputFileName += ".png";
 			}
 		} else {
 			transcoder = new org.apache.batik.transcoder.image.JPEGTranscoder();
-			transcoder.addTranscodingHint(org.apache.batik.transcoder.image.JPEGTranscoder.KEY_QUALITY, new Float(.8));
-        	if(!outputFileName.toLowerCase().endsWith(".jpg") && !outputFileName.toLowerCase().endsWith(".jpeg")) {
+			transcoder.addTranscodingHint(org.apache.batik.transcoder.image.JPEGTranscoder.KEY_QUALITY, new Float(1.0));
+			if(!outputFileName.toLowerCase().endsWith(".jpg") && !outputFileName.toLowerCase().endsWith(".jpeg")) {
 				outputFileName += ".jpg";
 			}
 		}
 		int columnWidth = 8;
 		int rowWidth = 8;
-		
-		for(int i = 3; i < args.length; ) { // 1st arg is input file name, 2nd arg is output file name, 3rd arg is format
+
+		for(int i = 2; i < args.length; i++) { // 1st arg is input file name, 2nd arg is output file name, 3rd arg is format
 			if(args[i].equals("-cw")) {
 				columnWidth = Integer.parseInt(args[++i]);
-				break;
-			}else if(args[i].equals("-rw")) {
+			} else if(args[i].equals("-rw")) {
 				rowWidth = Integer.parseInt(args[++i]);
-				break;
 			} else {
 				System.err.println("unknown option " + args[i]);
 				System.exit(1);
 			}
 		}
 		BufferedImage snapshotImage = hcl.heatMapSnapshot(transcoder, columnWidth, rowWidth);
-        FileOutputStream fos = new FileOutputStream(outputFileName);
-        org.apache.batik.transcoder.TranscoderOutput out = new org.apache.batik.transcoder.TranscoderOutput(fos);
-        transcoder.writeImage(snapshotImage, out);
-        fos.close();
+		FileOutputStream fos = new FileOutputStream(outputFileName);
+		org.apache.batik.transcoder.TranscoderOutput out = new org.apache.batik.transcoder.TranscoderOutput(fos);
+		transcoder.writeImage(snapshotImage, out);
+		fos.close();
 		// saving image requires usually requires at least 512MB of memory
-	/*	BufferedImage snaphotImage = hcl.heatMapSnapshot();
-		
-		ij.ImagePlus ip = new ij.ImagePlus();
-		ip.setImage(snaphotImage);
-		ij.io.FileSaver fs = new ij.io.FileSaver(ip);
-		
-		String outputFormat = args[2];
-		String outputFileName = args[1];
-		if(outputFormat.equals("tiff")) {
-			if(!outputFileName.toLowerCase().endsWith(".tiff")) {
-				outputFileName += ".tiff";
-			}
-			fs.saveAsTiff(outputFileName);
-		} else if(outputFormat.equals("bmp")) {
-			if(!outputFileName.toLowerCase().endsWith(".bmp")) {
-				outputFileName += ".bmp";
-			}
-			fs.saveAsBmp(outputFileName);
-		
-		} else {
-			if(!outputFileName.toLowerCase().endsWith(".jpg") && !outputFileName.toLowerCase().endsWith(".jpeg")) { // fails on windows
-				outputFileName += ".jpg";
-			}
-			fs.saveAsJpeg(outputFileName);
-		}
-		*/
+		/*
+		 *  BufferedImage snaphotImage = hcl.heatMapSnapshot();
+		 *  ij.ImagePlus ip = new ij.ImagePlus();
+		 *  ip.setImage(snaphotImage);
+		 *  ij.io.FileSaver fs = new ij.io.FileSaver(ip);
+		 *  String outputFormat = args[2];
+		 *  String outputFileName = args[1];
+		 *  if(outputFormat.equals("tiff")) {
+		 *  if(!outputFileName.toLowerCase().endsWith(".tiff")) {
+		 *  outputFileName += ".tiff";
+		 *  }
+		 *  fs.saveAsTiff(outputFileName);
+		 *  } else if(outputFormat.equals("bmp")) {
+		 *  if(!outputFileName.toLowerCase().endsWith(".bmp")) {
+		 *  outputFileName += ".bmp";
+		 *  }
+		 *  fs.saveAsBmp(outputFileName);
+		 *  } else {
+		 *  if(!outputFileName.toLowerCase().endsWith(".jpg") && !outputFileName.toLowerCase().endsWith(".jpeg")) { // fails on windows
+		 *  outputFileName += ".jpg";
+		 *  }
+		 *  fs.saveAsJpeg(outputFileName);
+		 *  }
+		 */
 		System.exit(1);
 	}
-	
+
 }
 
 /*
-java -Xmx512m -cp <libdir>colt.jar<path.separator><libdir>file_support.jar<path.separator><libdir>gp-common.jar<path.separator><libdir>hclviewer.jar<path.separator><libdir>ij.jar<path.separator><libdir>trove.jar edu.mit.genome.gp.ui.hclviewer.HeatMap ~/res_files/ALL.res out jpeg
-*/
+ *  java -Xmx512m -cp <libdir>colt.jar<path.separator><libdir>file_support.jar<path.separator><libdir>gp-common.jar<path.separator><libdir>hclviewer.jar<path.separator><libdir>ij.jar<path.separator><libdir>trove.jar edu.mit.genome.gp.ui.hclviewer.HeatMap ~/res_files/ALL.res out jpeg
+ */
+
