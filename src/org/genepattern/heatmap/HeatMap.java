ccb9a50a9b3bcf552d6b72da9e34dd409894e8e3
nazaire <nazaire@47fccdf2-e235-0410-87e5-63258e95d6e5>
2010-03-19 17:47:45 +0000

    removed import of iOException HeatMap class
    git-svn-id: https://svn.broadinstitute.org/gp2/trunk/modules/HierarchicalClusteringViewer@18123 47fccdf2-e235-0410-87e5-63258e95d6e5

--- src/org/genepattern/heatmap/HeatMap.java
+++ src/org/genepattern/heatmap/HeatMap.java
@@ -30,7 +30,6 @@ import java.awt.event.MouseEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
